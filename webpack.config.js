e026ba9f37cbc83e231b639c4d72fd2ae7db564e
Olivia <livie7c@gmail.com>
2018-07-01 16:55:27 +0200

    Install and configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/scripts/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'src', 'temp', 'scripts'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+    ],
+  },
+  mode: 'production',
+};
