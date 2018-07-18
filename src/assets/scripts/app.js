10258650de506d4335deb997708ee662b12c409d
Olivia <livie7c@gmail.com>
2018-07-18 16:30:17 +0200

    Finish javascript for toggleable searchForm

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,3 +1,5 @@
 import MobileNav from './modules/MobileNav';
+import ToggleableSearchForm from './modules/ToggleableSearchForm';
 
 const mobileNav = new MobileNav();
+const searchForm = new ToggleableSearchForm();
---
+++ src/assets/scripts/modules/ToggleableSearchForm.js
@@ -0,0 +1,47 @@
+class ToggleableSearchForm {
+  constructor() {
+    this.searchForm = document.querySelector('.search-form');
+    this.toggleFormBtns = document.querySelectorAll('.toggle-form-btn');
+    this.searchIcons = document.querySelectorAll('.toggle-form-btn__search');
+    this.crossIcons = document.querySelectorAll('.toggle-form-btn__cross');
+    this.events();
+  }
+
+  events() {
+    const toggleBtns = Array.prototype.slice.call(this.toggleFormBtns);
+    toggleBtns.forEach(btn =>
+      btn.addEventListener('click', this.toggleForm.bind(this))
+    );
+  }
+
+  toggleForm() {
+    // turn node list into arrays
+    const searchIconArr = Array.prototype.slice.call(this.searchIcons);
+    const crossIconArr = Array.prototype.slice.call(this.crossIcons);
+
+    // for searchForm
+    if (this.searchForm.className === 'search-form') {
+      this.searchForm.className = 'search-form search-form--visible';
+      searchIconArr.forEach(
+        icon => (icon.className = 'icon-search toggle-form-btn__search')
+      );
+      crossIconArr.forEach(
+        icon =>
+          (icon.className =
+            'icon-cross toggle-form-btn__cross toggle-form-btn__cross--visible')
+      );
+    } else {
+      this.searchForm.className = 'search-form';
+      searchIconArr.forEach(
+        icon =>
+          (icon.className =
+            'icon-search toggle-form-btn__search toggle-form-btn__search--visible')
+      );
+      crossIconArr.forEach(
+        icon => (icon.className = 'icon-cross toggle-form-btn__cross')
+      );
+    }
+  }
+}
+
+export default ToggleableSearchForm;
